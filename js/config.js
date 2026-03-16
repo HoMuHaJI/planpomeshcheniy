fbf4ca5f5d6733751baad61529336ba8b2b9162b
HoMuHaJI <nikolay-k@bk.ru>
2026-03-16 20:11:31 +0300

    Delete js/config.js

--- js/config.js
+++
@@ -1,8 +0,0 @@
-window.TELEGRAM_CONFIG = {
-    BOT_TOKEN: '8142957488:AAGDsEIsGdtrCX-ZyvOD7nJjaVVD3_YIFks',
-    CHAT_ID: '-1001701431569'
-};
-
-window.getTelegramConfig = function() {
-    return { token: window.TELEGRAM_CONFIG.BOT_TOKEN, chatId: window.TELEGRAM_CONFIG.CHAT_ID };
-};
